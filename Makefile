48daf1f2d8702db2073285b74a862f6dfe7135fa
Rafael Mohlin <raffe_mohlin@hotmail.com>
2016-05-25 10:20:18 +0200

    initial code commit

---
+++ Makefile
@@ -0,0 +1,35 @@
+GCC = c++
+WARNINGS = -Wall -Werror
+OPTIONS = -m32 -pipe -msse -mfpmath=sse
+FLAGS = -fvisibility=hidden -fvisibility-inlines-hidden -fno-exceptions -fno-threadsafe-statics
+EXTRA_FLAGS = -fno-rtti
+DEFINES = -Dstricmp=strcasecmp -D_stricmp=strcasecmp -D_snprintf=snprintf \
+		  -D_vsnprintf=vsnprintf -DGNUC -D_LINUX -DPOSIX -DCOMPILER_GCC
+INCLUDES = -Ihl2sdk-csgo/public -Ihl2sdk-csgo/public/engine -Ihl2sdk-csgo/public/mathlib \
+		   -Ihl2sdk-csgo/public/tier0 -Ihl2sdk-csgo/public/tier1 -Ihl2sdk-csgo/public/game/server
+
+all: affesay.so
+
+affesay.so: affesay.o netmessages.pb.o cstrike15_usermessages.pb.o libtier0.so hl2sdk-csgo/lib/linux/tier1_i486.a hl2sdk-csgo/lib/linux/interfaces_i486.a
+	$(GCC) $^ $(OPTIONS) -static-libgcc -lstdc++ $(shell pkg-config --cflags --libs protobuf) -shared -o affesay.so
+
+affesay.o: affesay.cpp netmessages.pb.h cstrike15_usermessages.pb.h
+	$(GCC) affesay.cpp $(WARNINGS) $(OPTIONS) $(FLAGS) $(EXTRA_FLAGS) $(DEFINES) $(INCLUDES) -c -o affesay.o
+
+%.pb.o: %.pb.cc
+	$(GCC) $^ $(WARNINGS) $(OPTIONS) $(FLAGS) -c -o $@
+
+%.pb.h %.pb.cc: %.proto
+	protoc --proto_path=. --proto_path=/usr/include --cpp_out=. $^
+
+libtier0.so: hl2sdk-csgo/lib/linux/libtier0.so
+	cp $^ $@
+
+netmessages.proto: hl2sdk-csgo/public/engine/protobuf/netmessages.proto
+	cp $^ $@
+
+cstrike15_usermessages.proto: hl2sdk-csgo/public/game/shared/csgo/protobuf/cstrike15_usermessages.proto
+	cp $^ $@
+
+clean:
+	rm -rf *.o *.so *.pb.h *.pb.cc *.proto
---
+++ affesay.cpp
@@ -0,0 +1,181 @@
+#include <iserverplugin.h>
+#include <eiface.h>
+#include <iplayerinfo.h>
+#include <irecipientfilter.h>
+#include <tier1.h>
+#include "cstrike15_usermessages.pb.h"
+
+class ChatFilter : public IRecipientFilter
+{
+    private:
+        int recipient;
+    public:
+        ChatFilter(int rec)
+        {
+            recipient = rec;
+        }
+        virtual bool	IsReliable( void ) const
+        {
+            return true;
+        }
+        virtual bool	IsInitMessage( void ) const
+        {
+            return false;
+        }
+
+        virtual int		GetRecipientCount( void ) const
+        {
+            return 1;
+        }
+        virtual int		GetRecipientIndex( int slot ) const
+        {
+            return  recipient;
+        }
+};
+
+class ServerPluginCallbacks : public IServerPluginCallbacks
+{
+    private:
+        IVEngineServer *engine = NULL;
+        ICvar *icvar = NULL;
+        IPlayerInfoManager *playerinfomanager = NULL;
+        CGlobalVars *globals = NULL;
+    public:
+	// Initialize the plugin to run
+	// Return false if there is an error during startup.
+    virtual bool Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory)
+    {
+        ConnectTier1Libraries(&interfaceFactory, 1);
+        engine = (IVEngineServer *) interfaceFactory(INTERFACEVERSION_VENGINESERVER, NULL);
+        if(!engine)
+            ConMsg("No engine\n");
+        icvar = (ICvar *) interfaceFactory(CVAR_INTERFACE_VERSION, NULL);
+        if(!icvar)
+            ConMsg("No icvar\n");
+        playerinfomanager = (IPlayerInfoManager *) gameServerFactory(INTERFACEVERSION_PLAYERINFOMANAGER, NULL);
+        if(!playerinfomanager)
+            ConMsg("No playerinfomanager\n");
+        globals = playerinfomanager->GetGlobalVars();
+        return true;
+    }
+
+	// Called when the plugin should be shutdown
+	virtual void			Unload( void )
+    {
+    }
+
+	// called when a plugins execution is stopped but the plugin is not unloaded
+	virtual void			Pause( void )
+    {
+    }
+
+	// called when a plugin should start executing again (sometime after a Pause() call)
+	virtual void			UnPause( void )
+    {
+    }
+
+	// Returns string describing current plugin.  e.g., Admin-Mod.  
+	virtual const char     *GetPluginDescription( void )
+    {
+        return "MrAffe";
+    }
+
+	// Called any time a new level is started (after GameInit() also on level transitions within a game)
+	virtual void			LevelInit( char const *pMapName )
+    {
+    }
+
+	// The server is about to activate
+	virtual void			ServerActivate( edict_t *pEdictList, int edictCount, int clientMax )
+    {
+    }
+
+	// The server should run physics/think on all edicts
+	virtual void			GameFrame( bool simulating )
+    {
+    }
+
+	// Called when a level is shutdown (including changing levels)
+	virtual void			LevelShutdown( void )
+    {
+    }
+
+	// Client is going active
+	virtual void			ClientActive( edict_t *pEntity )
+    {
+    }
+
+	// Client is fully connected ( has received initial baseline of entities )
+	virtual void			ClientFullyConnect( edict_t *pEntity )
+    {
+    }
+
+	// Client is disconnecting from server
+	virtual void			ClientDisconnect( edict_t *pEntity )
+    {
+    }
+	
+	// Client is connected and should be put in the game
+	virtual void			ClientPutInServer( edict_t *pEntity, char const *playername )
+    {
+    }
+
+	// Sets the client index for the client who typed the command into their console
+	virtual void			SetCommandClient( int index )
+    {
+    }
+
+	// A player changed one/several replicated cvars (name etc)
+	virtual void			ClientSettingsChanged( edict_t *pEdict )
+    {
+    }
+
+	// Client is connecting to server ( set retVal to false to reject the connection )
+	//	You can specify a rejection message by writing it into reject
+	virtual PLUGIN_RESULT	ClientConnect( bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *reject, int maxrejectlen )
+    {
+        return PLUGIN_CONTINUE;
+    }
+
+	// The client has typed a command at the console
+	virtual PLUGIN_RESULT	ClientCommand( edict_t *pEdict, const CCommand &args )
+    {
+        int entindex = pEdict - globals->pEdicts;
+        CCSUsrMsg_SayText msg;
+        ChatFilter filter(entindex);
+
+        ConMsg("COMMAND: %s///\n", args.GetCommandString());
+        if(!strcasecmp("affe ", args.GetCommandString())) {
+                ConMsg("SENDING STUFF\n");
+                msg.set_text("t\x01je\x02n\x03na");
+                msg.set_chat(true);
+                engine->SendUserMessage(filter, CS_UM_SayText, msg);
+        }
+        engine->ClientPrintf(pEdict, "T\x01j\x02e\x03n\x03n\x04a\a");
+        return PLUGIN_CONTINUE;
+    }
+
+	// A user has had their network id setup and validated 
+	virtual PLUGIN_RESULT	NetworkIDValidated( const char *pszUserName, const char *pszNetworkID )
+    {
+        return PLUGIN_CONTINUE;
+    }
+
+	// This is called when a query from IServerPluginHelpers::StartQueryCvarValue is finished.
+	// iCookie is the value returned by IServerPluginHelpers::StartQueryCvarValue.
+	// Added with version 2 of the interface.
+	virtual void			OnQueryCvarValueFinished( QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue )
+    {
+    }
+
+	// added with version 3 of the interface.
+	virtual void			OnEdictAllocated( edict_t *edict )
+    {
+    }
+	virtual void			OnEdictFreed( const edict_t *edict  )
+    {
+    }	
+};
+
+ServerPluginCallbacks g_Plugin;
+EXPOSE_SINGLE_INTERFACE_GLOBALVAR(ServerPluginCallbacks, IServerPluginCallbacks, INTERFACEVERSION_ISERVERPLUGINCALLBACKS, g_Plugin );
